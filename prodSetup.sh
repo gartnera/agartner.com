4a40d8aba27785104074a0c5b7df63d9f6f6990d
Alex Gartner <alex@agartner.com>
2014-07-24 15:09:55 -0500

    Changed font stylesheet to https. Removed .empty because it was interpreted a s post. Added logic to manually make posts directories to prodSetup.sh. Fixed prodSetup paths.

--- prodSetup.sh
+++ prodSetup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 for file in $(find $(find * -maxdepth 0 -type d) -type f); do
-        sed -i 's|href="blog/"|href="https://blog.agartner.com/"|g' $file;
-        sed -i 's|href="profile/"|href="https://profile.agartner.com/"|g' $file;
+        sed -i 's|href="/blog"|href="https://blog.agartner.com/"|g' $file;
+        sed -i 's|href="/profile"|href="https://profile.agartner.com/"|g' $file;
 done;
 
 sed -i 's|AuthUserFile.*|AuthUserFile /var/www/blog/admin/.htpasswd|' /var/www/blog/admin/.htaccess
+
+mkdir -p blog/posts/json
