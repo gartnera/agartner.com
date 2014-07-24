b54eb3861a67450afbd35597e8e41e6b149e6f42
Alex Gartner <alex@agartner.com>
2014-07-24 14:53:23 -0500

    Fixed bad find statement

--- prodSetup.sh
+++ prodSetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for file in $(find -type f); do
-        sed -i 's|https://blog.agartner.com|https:/https://blog.agartner.com.agartner.com|g' $file;
-        sed -i 's|https://profile.agartner.com|https:/https://profile.agartner.com.agartner.com|g' $file;
+for file in $(find $(find * -maxdepth 0 -type d) -type f); do
+        sed -i 's|href="blog/"|href="https://blog.agartner.com/"|g' $file;
+        sed -i 's|href="profile/"|href="https://profile.agartner.com/"|g' $file;
 done;
 
 sed -i 's|AuthUserFile.*|AuthUserFile /var/www/admin/.htpasswd|' /var/www/admin/.htaccess
