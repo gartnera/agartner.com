490cec507b59af61c9be1e5cb05adce53bcb0be0
Alex Gartner <alex@agartner.com>
2014-07-24 14:58:01 -0500

    Fixed bad .htpasswd path

--- prodSetup.sh
+++ prodSetup.sh
@@ -5,4 +5,4 @@ for file in $(find $(find * -maxdepth 0 -type d) -type f); do
         sed -i 's|href="profile/"|href="https://profile.agartner.com/"|g' $file;
 done;
 
-sed -i 's|AuthUserFile.*|AuthUserFile /var/www/admin/.htpasswd|' /var/www/admin/.htaccess
+sed -i 's|AuthUserFile.*|AuthUserFile /var/www/blog/admin/.htpasswd|' /var/www/blog/admin/.htaccess
